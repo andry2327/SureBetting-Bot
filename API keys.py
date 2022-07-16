a9aea6c500ff923601301fe707f9a0b7e8a88e48
andry2327 <andry2327@gmail.com>
2022-07-16 18:24:58 +0200

    first commit

---
+++ API keys.py
@@ -0,0 +1,11 @@
+# API keys form https://the-odds-api.com/
+
+req_month = 500
+n_keys = 2
+
+TOT_req_month = n_keys*req_month
+
+key1 = 'YOUR_API_KEY_1' 
+key2 = 'YOUR_API_KEY_2' # ...you can add as many api keys as you need...
+
+
