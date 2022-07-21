b64a31945d5e8aac95e7dd2e5deef3f74c64d900
andry2327 <andry2327@gmail.com>
2022-07-21 10:20:16 +0200

    get_sports() correction

--- functions.py
+++ functions.py
@@ -2,8 +2,8 @@ from my_API_keys import keys_list
 from requests import get
 import json
 
-def get_sports(APY_KEY):
-    url = 'https://api.the-odds-api.com/v4/sports/?apiKey={}'.format(keys_list[0])
+def get_sports(API_KEY):
+    url = 'https://api.the-odds-api.com/v4/sports/?apiKey={}'.format(API_KEY)
     sports = get(url)
     sports = sports.content.decode('utf-8')
     return json.loads(sports)
