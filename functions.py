9ca3736a37c77cc78c9737e80c77d9894624c728
andry2327 <andry2327@gmail.com>
2022-07-21 10:32:29 +0200

    get_odds() and get_scores()

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-from my_API_keys import keys_list
 from requests import get
 import json
 
@@ -7,3 +6,19 @@ def get_sports(API_KEY):
     sports = get(url)
     sports = sports.content.decode('utf-8')
     return json.loads(sports)
+
+def get_odds(API_KEY, region, market):
+    url = 'https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions={}&markets={}&apiKey={}'.format(region, market, API_KEY)
+    odds = get(url)
+    odds = odds.content.decode('utf-8')
+    return json.loads(odds)
+
+def get_scores(API_KEY, sport, days_from):  # set days_from = 0 if you don' need this option
+    if(days_from==0):
+        url = 'https://api.the-odds-api.com/v4/sports/YOUR_SPORT_KEY/scores/?apiKey=YOUR_API_KEY'.format(sport, API_KEY)
+    else:
+        url = 'https://api.the-odds-api.com/v4/sports/YOUR_SPORT_KEY/scores/?daysFrom=YOUR_DAYS_FROM&apiKey=YOUR_API_KEY'.format(sport, days_from, API_KEY)
+
+    scores = get(url)
+    scores = scores.content.decode('utf-8')
+    return json.loads(scores)
