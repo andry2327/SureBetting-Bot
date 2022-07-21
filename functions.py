cb84b7cf89e03c46b050bcb400f3994c11d0a2cc
andry2327 <andry2327@gmail.com>
2022-07-21 19:28:28 +0200

    split functions: API and json format + API usage

--- functions.py
+++ functions.py
@@ -1,27 +1,41 @@
 from requests import get
 import json
 
-# functions to use APIs
+# HTML API response 
 
-def get_sports(API_KEY):
+def get_API_response(API_KEY):
     url = 'https://api.the-odds-api.com/v4/sports/?apiKey={}'.format(API_KEY)
-    sports = get(url)
-    sports = sports.content.decode('utf-8')
-    return json.loads(sports)
+    response = get(url)
+    return response
 
-def get_odds(API_KEY, region, market):
-    url = 'https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions={}&markets={}&apiKey={}'.format(region, market, API_KEY)
-    odds = get(url)
-    odds = odds.content.decode('utf-8')
-    return json.loads(odds)
+def get_API_response_Sports(API_KEY):
+    url = 'https://api.the-odds-api.com/v4/sports/?apiKey={}'.format(API_KEY)
+    response = get(url)
+    return response
 
-def get_scores(API_KEY, sport, days_from):  # set days_from = 0 if you don' need this option
-    if(days_from==0):
+def get_API_response_Odds(API_KEY, region, market):
+    url = 'https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions={}&markets={}&apiKey={}'.format(
+        region, market, API_KEY)
+    response = get(url)
+    return response
+
+def get_API_response_Scores(API_KEY, sport, days_from):  # set days_from = 0 if you don' need this option
+    if(days_from == 0):
         url = 'https://api.the-odds-api.com/v4/sports/{}/scores/?apiKey={}'.format(sport, API_KEY)
     else:
         url = 'https://api.the-odds-api.com/v4/sports/{}/scores/?daysFrom={}&apiKey={}'.format(sport, days_from, API_KEY)
-    scores = get(url)
-    scores = scores.content.decode('utf-8')
-    return json.loads(scores)
+    response = get(url)
+    return response
 
+# .json data from html API response
+
+def response_to_json(response):
+    respones = response.content.decode('utf-8')
+    return json.loads(response)
+
+# get API_KEY remaining requests
+
+def remaning_requests(response):
+    headers = response.headers
+    return int(headers['X-Requests-Remaining'])
 
--- main.py
+++ main.py
@@ -5,4 +5,13 @@ regions = 'eu' # I only use regions which i have access to
 oddsFormat = 'decimal'
 markets = ['h2h', 'totals'] # I will only use these two type of bets to make it easier, beacuse they are binary bets (win/lose, over/under), you can find bets explaination in 'utility.md'
 
+i = 0
 
+# after done: while(1):
+
+# get live event odds
+for i in range (0, len(keys_list)):
+    pass
+
+    
+    
