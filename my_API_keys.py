679b5a659ad5e59c540087a43e7a9c81c8624833
andry2327 <andry2327@gmail.com>
2022-08-17 15:23:25 +0200

    filter % proft

---
+++ my_API_keys.py
@@ -0,0 +1,36 @@
+# API keys form https://the-odds-api.com/
+
+req_month = 500
+n_keys = 20
+
+TOT_req_month = n_keys*req_month
+
+key1 = 'c97152de220527865b5477a83b035b5c' # andry2327@gmail.com
+key2 = 'dbadaa2263d01fb7d0ca13a7a71dc5cd' # temp mail: koniw47191@tagbert.com
+key3 = '62fb2b90d0004beb1367e3de95e6930e' # temp mail: ronejek436@tebyy.com
+key4 = '41dd0f266e065c65f2102b6810f4172e'
+key5 = '8eadc70806c4d542ce583a8ed5040d99'
+key6 = '3339647b0f044ebbaf43615e38474ef3'
+key7 = '5df8d069b16e23263200f3b77860c35e'
+key8 = '3ad6c7072bc04fdbd8efbfa7a2ea1040'
+key9 = 'e4f672471bb369166b0d79d022f84d0a'
+key10 = '3a3e5ffa387beb185977df946fd1ba93'
+key11 = '22880423ac12c8ecad2a4fc5a76bb1ec'
+key12 = 'c701063ed542d3e1d55d34304dfdba2b'
+key13 = '131c08e2a980023f8229251292630917'
+key14 = '3ec726c3d7daa72916f24ddcf1712ef9'
+key15 = '2cc02328ee22a8976d727ef5edfb06b8'
+key16 = 'a4359aae9a264e24a253438166d0abe9'
+key17 = 'cb2313ba5bf9e480ee2599ff304d7f38'
+key18 = 'c4b53aa4faa6f654ab8b41cb1d3224d1'
+key19 = 'f38240e67995d64d30629f1ffcc719cd'
+key20 = '39b33e335264bd86d97aa6eaed22fe74'
+
+keys_list = [key1, key2, key3, key4, key5,
+             key6, key7, key8, key9, key10,
+             key11, key12, key13, key14, key15, 
+             key16, key17, key18, key19, key20]
+
+apdb_API_key = 'beeb3ea9-360e-4324-b531-02d28143411e' # 10k req/month
+token = '5532535981:AAGhAITVjrluni05EkyJfB5Fva0lHoyqsrI'  # Telegram bot token
+chat_id = -1001712610006
